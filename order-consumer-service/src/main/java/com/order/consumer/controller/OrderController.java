1850eff0128ef2770bba396d7ecd1c218a650709
sddking <794279085@qq.com>
2020-07-06 22:29:12 +0800

    eleventh commit

--- order-consumer-service/src/main/java/com/order/consumer/controller/OrderController.java
+++ order-consumer-service/src/main/java/com/order/consumer/controller/OrderController.java
@@ -1,5 +1,7 @@
 package com.order.consumer.controller;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
@@ -24,11 +26,21 @@ public class OrderController {
         return  restTemplate.getForObject(serverURL+"/payment/get/"+id,String.class);
     }
 
-    @GetMapping(value = "/test")
+    //供热点规则的资源名使用
+    //当不满足热点规则时，由blockHandler兜底
+    //参数索引从零开始,例如sentinel中设置的阈值为1且参数的索引设置为0，当QPS超过1时则立即触发BlockException
+    // localhost:8003/testHotKey?p1=a           受限制
+    // localhost:8003/testHotKey?p1=a&p2=b      受限制
+    // localhost:8003/testHotKey?p2=b           不受限制
+    @GetMapping(value = "/testHotKey")
+    @SentinelResource(value = "testHotKey",blockHandler = "deal_testHotKey")
     public String testHotKey(
             @RequestParam(value = "p1",required = false)String p1,
             @RequestParam(value = "p2",required = false)String p2
     ){
         return "-------testHotKey----------";
+    }
+    public String deal_testHotKey(String p1, String p2, BlockException blockException){
+        return "................哭脸";
     }
 }
